0f50605e8ba406aa9fc93cc3a0ce3858ed52ca95
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-03-21 18:00:02 +0300

    Moving on to making the search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <section className="pink_container bg-gradient-to-r from-black-200 via-red-600 to-green-600">
+      <section className="pink_container pattern bg-gradient-to-r from-black-200 via-red-600 to-green-600">
 
-    <h1 className="heading">Pitch Your Idea</h1>
+    <h1 className="heading">Pitch Your Idea<br/>Connect with Your Future </h1>
+
+    <p className="sub-heading !max-w-3xl">
+      Submit Ideas, Gauge The Pitches, and Get Noticed in Virtual Competitions. 
+    </p>
       </section>
     </>
   );
