b48cb1af1ffdf44193f649b06172dbc3950291a9
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-03-21 18:27:23 +0300

    Now making the search bar more functional in order to change the url

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,23 +1,16 @@
 import Form from "next/form"
+import SearchFormReset from "./SearchFormReset"
 const SearchForm = () => {
   const query = 'Test'
 
-  const reset = () => {
-    const form = document.querySelector('.search-form') as HTMLFormElement;
-
-    if(form) form.reset();
-  }
 
   return (
     <Form action="/" scroll={false} className="search-form">
       <input name="query" defaultValue={query} className="search-input" placeholder="Search Startups" />
 
       <div className="flex gap-2">
-        {query && (
-          <button type="reset" onClick={reset} >
-
-          </button>
-        )}
+        {query && <SearchFormReset/>}
+        <button type="submit" className="search-btn text-white">S</button>
       </div>
     </Form>
   )
