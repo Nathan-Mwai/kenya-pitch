85ff0c169e62679fbd03ec6d4286a211237e510a
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-03-21 18:17:04 +0300

    Made a searchform

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,21 @@
+import SearchForm from "@/components/SearchForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <section className="pink_container pattern bg-gradient-to-r from-black-200 via-red-600 to-green-600">
+      <section className="pink_container bg-gradient-to-r from-black-200 via-red-600 to-green-600 pattern">
+        <h1 className="heading">
+          Pitch Your Idea
+          <br />
+          Connect with Your Future{" "}
+        </h1>
 
-    <h1 className="heading">Pitch Your Idea<br/>Connect with Your Future </h1>
-
-    <p className="sub-heading !max-w-3xl">
-      Submit Ideas, Gauge The Pitches, and Get Noticed in Virtual Competitions. 
-    </p>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Gauge The Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm/>
       </section>
     </>
   );
