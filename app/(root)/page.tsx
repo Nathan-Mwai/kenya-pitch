319ce3aa5a831c936cdc3847c6f2b319db5d5298
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-03-21 17:48:44 +0300

    Still on the cat

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 className="heading">Home</h1>
+      <section className="pink_container bg-gradient-to-r from-black-200 via-red-600 to-green-600">
+
+    <h1 className="heading">Pitch Your Idea</h1>
+      </section>
     </>
   );
 }
