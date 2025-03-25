ee00874a6dec0b45fd637fb35e009b8d5e220a26
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-03-25 15:52:12 +0300

    Set up sanity moving on to setting the sanity schemas

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,26 @@
 import SearchForm from "@/components/SearchForm";
-import Image from "next/image";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}: {
   searchParams: Promise<{query?:string}>
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Nathan" },
+      _id: 1,
+      description: "This is a description",
+      image:"https://images.unsplash.com/photo-1534723328310-e82dad3ee43f?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category:"Robots",
+      title: "We Robots",
+    }
+  ];
   return (
     <>
-      <section className="pink_container bg-gradient-to-r from-black-200 via-red-600 to-green-600 pattern">
+      <section className="pink_container bg-gradient-to-r from-black-200 via-red-600 to-green-600">
         <h1 className="heading">
           Pitch Your Idea
           <br />
@@ -19,6 +32,22 @@ export default async function Home({searchParams}: {
           Competitions.
         </p>
         <SearchForm query={query}/>
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}" `: 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType)=>(
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ): (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,5 +1,6 @@
 import Form from "next/form"
 import SearchFormReset from "./SearchFormReset"
+import { Search } from "lucide-react"
 const SearchForm = ({query}:{query?:string}) => {
 
   return (
@@ -8,7 +9,9 @@ const SearchForm = ({query}:{query?:string}) => {
 
       <div className="flex gap-2">
         {query && <SearchFormReset/>}
-        <button type="submit" className="search-btn text-white">S</button>
+        <button type="submit" className="search-btn text-white">
+          <Search className="size-5"/>
+        </button>
       </div>
     </Form>
   )
