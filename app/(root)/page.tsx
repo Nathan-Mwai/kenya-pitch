c81141f86170db1f72446c8b86d14123d1e94e00
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-03-26 17:01:22 +0300

    Managed to enable viewing of the data from being private to public so that it can be accessible to all including from localhost since even in production it will be public

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,31 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({searchParams}: {
   searchParams: Promise<{query?:string}>
 }) {
   const query = (await searchParams).query;
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name: "Nathan" },
-      _id: 1,
-      description: "This is a description",
-      image:"https://images.unsplash.com/photo-1534723328310-e82dad3ee43f?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category:"Robots",
-      title: "We Robots",
-    }
-  ];
+  const posts =  await client.fetch(STARTUPS_QUERY);
+
+  // console.log(JSON.stringify(posts));
+  
+
+  // const posts = [
+  //   {
+  //     _createdAt: new Date(),
+  //     views: 55,
+  //     author: { _id: 1, name: "Nathan" },
+  //     _id: 1,
+  //     description: "This is a description",
+  //     image:"https://images.unsplash.com/photo-1534723328310-e82dad3ee43f?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //     category:"Robots",
+  //     title: "We Robots",
+  //   }
+  // ];
+  
   return (
     <>
       <section className="pink_container bg-gradient-to-r from-black-200 via-red-600 to-green-600">
