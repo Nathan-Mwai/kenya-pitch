cf1b4174778a70cb280ada5fa22a1fa4d4fc3cb4
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2025-04-14 10:49:38 +0300

    Implement data fetching as well as increasing type safety of project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
-
 export default async function Home({searchParams}: {
   searchParams: Promise<{query?:string}>
 }) {
@@ -10,22 +9,6 @@ export default async function Home({searchParams}: {
 
   const posts =  await client.fetch(STARTUPS_QUERY);
 
-  // console.log(JSON.stringify(posts));
-  
-
-  // const posts = [
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: { _id: 1, name: "Nathan" },
-  //     _id: 1,
-  //     description: "This is a description",
-  //     image:"https://images.unsplash.com/photo-1534723328310-e82dad3ee43f?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //     category:"Robots",
-  //     title: "We Robots",
-  //   }
-  // ];
-  
   return (
     <>
       <section className="pink_container bg-gradient-to-r from-black-200 via-red-600 to-green-600">
@@ -49,7 +32,7 @@ export default async function Home({searchParams}: {
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType)=>(
+            posts.map((post: StartupTypeCard)=>(
               <StartupCard key={post?._id} post={post} />
             ))
           ): (
